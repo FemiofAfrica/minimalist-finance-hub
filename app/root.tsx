98337053fa8fb3fde80756a76965c4ec3d9135f7
femiofafrica <phermmodynamic@gmail.com>
2025-05-30 13:23:07 +0100

    Add Google Analytics Tracking to Application
    - Integrated Google Tag Manager (gtag.js) for enhanced tracking and analytics.
    - Added necessary scripts to initialize Google Analytics with the specified tracking ID.
    
    This commit improves the application's analytics capabilities, allowing for better user behavior tracking and insights.

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,16 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MYJ82B2K0X"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MYJ82B2K0X');
+          `
+        }}></script>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
